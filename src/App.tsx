3b53b9dccb266b5a561cda7f1e92196dfdef6074
Delsivan Nascimento Vieira <delsivannascimentovieira@gmail.com>
2022-03-22 23:18:58 -0300

    Criando o componente Lista utilizando function component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Formulario from './components/Formulario';
+import Lista from './components/Formulario';
 
 
 function App() {
   return (
     <div className="App">
       <Formulario />
+      <Lista />
     </div>
   );
 }
