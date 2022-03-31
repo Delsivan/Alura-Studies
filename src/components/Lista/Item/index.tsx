4277b46e25b9a3bf2ec704776c7d757f2260c3cc
Delsivan Nascimento Vieira <delsivannascimentovieira@gmail.com>
2022-03-30 21:44:22 -0300

    Refatorando a lista

---
+++ src/components/Lista/Item/index.tsx
@@ -0,0 +1,11 @@
+import style from '../Lista.module.scss';
+
+export default function Item(props: {tarefa: string, tempo: string }) {
+    const { tarefa, tempo } = props;
+    return (
+        <li className={style.item}>
+            <h3> {tarefa} </h3>
+            <span> {tempo} </span>
+        </li>
+    )
+}
